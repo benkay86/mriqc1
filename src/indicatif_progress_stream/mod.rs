62abb81beaca2637d4ec79179ad17d380cf118ad
Benjamin Kay <benjamin@benkay.net>
2021-02-11 10:02:24 -0600

    Merge upstream indicatif_progress_stream

--- src/indicatif_progress_stream/mod.rs
+++ src/indicatif_progress_stream/mod.rs
@@ -2,7 +2,7 @@
 //! using `ProgressStream::progress_with()`, analagous to using
 //! `ProgressIterator::progress_with()` on an iterator.
 //! Uses GitHub gist as git submodule:
-//! https://gist.github.com/benkay86/6afffd4cf90ad84ac43e42d59d197e08
+//! <https://gist.github.com/benkay86/6afffd4cf90ad84ac43e42d59d197e08>
 
 // Allow unused code in this library-like module.
 #![allow(dead_code)]
